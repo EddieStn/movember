e6c9d7d2ec6d9f882b701a7993992f12a70a2c42
EddieStn <104385493+EddieStn@users.noreply.github.com>
2023-11-22 22:40:51 +0000

    21 edit blog (#72)
    * Create form for blog creation
    
    * Add views/url/template to create blogs
    
    * Create blog edit View/url/template
    
    * Create blog delete view/url/template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Blog
 from .forms import BlogForm
@@ -15,8 +15,7 @@ def create_blog(request):
     else:
         form = BlogForm()
 
-    context = {'form': form}
-    return render(request, 'blog/create_blog.html', context)
+    return render(request, 'blog/create_blog.html', {'form': form})
 
 
 @login_required
@@ -25,5 +24,48 @@ def blog_list(request):
     A view to render all the blogs on the blog page
     """
     blogs = Blog.objects.all()
-    context = {'blogs': blogs}
-    return render(request, 'blog/blog_list.html', context)
+    return render(request, 'blog/blog_list.html', {'blogs': blogs})
+
+
+@login_required
+def blog_detail(request, blog_id):
+    """
+    A view to display the blog detail when a user
+    clicks on it so they can be able to edit/delete
+    """
+    blog = get_object_or_404(Blog, pk=blog_id)
+    user = request.user
+
+    return render(request, 'blog/blog_detail.html', {'blog': blog, 'user': user})
+
+
+@login_required
+def edit_blog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    if blog.author != request.user:
+        return render(request, 'blog/unauthorized_access.html')
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+
+    return render(request, 'blog/edit_blog.html', {'form': form, 'blog': blog})
+
+
+@login_required
+def delete_blog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    if blog.author != request.user:
+        return render(request, 'blog/unauthorized_access.html')
+
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('blog_list')
+
+    return render(request, 'blog/delete_blog.html', {'blog': blog})
