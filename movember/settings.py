b7889c421e4e345894a01645a0db658a78684c3a
Eddie Stan <eduard.eduard25@yahoo.com>
2023-11-24 19:20:34 +0000

    Update settings

--- movember/settings.py
+++ movember/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'movember1-bac9d403956e.herokuapp.com']
 
