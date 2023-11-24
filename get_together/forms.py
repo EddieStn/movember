9ad431e37567156b4f16566925240701085c24fb
Joy Zadan <joyzadan.learning@gmail.com>
2023-11-24 12:45:50 +0000

    add changes to models, create event forms and rendering

--- get_together/forms.py
+++ get_together/forms.py
@@ -1,13 +1,39 @@
 from django import forms
-from .models import Category, GetTogether
+from djrichtextfield.widgets import RichTextWidget
+from .models import GetTogether
 
 
 class GetTogetherForm(forms.ModelForm):
+    """ Form to create a get together event """
 
-    new_category = forms.CharField(max_length=100, required=False, help_text="This will be a new category.")
+    new_category = forms.CharField(max_length=100, required=False,
+                                   help_text="This will be a new category.")
     class Meta:
         model = GetTogether
-        fields = ['title', 'category', 'new_category', 'description', 'date', 'time', 'location', 'max_participants', 'signup_deadline', 'image', 'image_alt']
+        fields = ['title', 'category', 'new_category', 'event_type',
+                  'description', 'date', 'time', 'location', 'max_participants',
+                  'signup_deadline', 'image', 'image_alt']
+
+    description = forms.CharField(widget=RichTextWidget())
+
+    widget = {
+        'description': forms.Textarea(attrs={"rows": 5}),
+    }
+
+    labels = {
+        'title': 'Get Together Title',
+        'category': 'Event or Interest Category',
+        'new_category': 'Add new category only if event or interest is not yet in Categories',
+        'description': 'Describe your get together to encourage the community members to join',
+        'date': "Date of Get Together",
+        'time': 'Time of Get Together',
+        'location': 'Location',
+        'max_participants': 'Maximum number of participants',
+        'signup_deadline': 'Signup Deadline',
+        'image': 'Upload an image, if available',
+        'image_alt': 'Describe your image to help make it accessible to everyone',
+        'event_type': 'Select Event Type',
+    }
 
     def __init__(self, *args, **kwargs):
         super(GetTogetherForm, self).__init__(*args, **kwargs)
--- get_together/models.py
+++ get_together/models.py
@@ -7,22 +7,37 @@ from django_resized import ResizedImageField
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        """ Meta class for Category model """
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
     def __str__(self):
         return self.name
+
+# Choice Fields
+EVENT_TYPES = (
+    ("face_to_face", "Face to Face"),
+    ("hybrid_event", "Hybrid Event"),
+    ("virtual_event", "Virtual Event"),
+)
 
 
 class GetTogether(models.Model):
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
+    event_type = models.CharField(max_length=50, blank=True, null=True, choices=EVENT_TYPES, default="face_to_face")
+    organizer = models.ForeignKey(User, related_name='organized_get_togethers', on_delete=models.CASCADE)
     description = RichTextField(max_length=500, null=True, blank=True)
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=200)
-    image = ResizedImageField(size=[100, 200],
-                              quality=75, upload_to='profiles/',
+    image = ResizedImageField(size=[300, None],
+                              quality=75, upload_to='get_together/',
                               force_format='WEBP', blank=True)
     image_alt = models.CharField(max_length=100, null=True, blank=True, default='event_image')
-    organizer = models.ForeignKey(User, related_name='organized_get_togethers', on_delete=models.CASCADE)
+
     participants = models.ManyToManyField(User, related_name='joined_get_togethers', blank=True)
     max_participants = models.PositiveIntegerField(default=10)
     signup_deadline = models.DateTimeField()
