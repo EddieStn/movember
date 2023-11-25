e35d8ed5ebe4caaed8304807a13b1419442493e4
Eddie Stan <eduard.eduard25@yahoo.com>
2023-11-25 10:46:42 +0000

    Style 404 500 pages

--- movember/settings.py
+++ movember/settings.py
@@ -30,7 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'movember1-bac9d403956e.herokuapp.com', '*.herokuapp.com']
 
