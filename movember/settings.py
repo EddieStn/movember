9104ce853bcebc4041ae6a0cc6b7c84da95f656a
Eddie Stan <eduard.eduard25@yahoo.com>
2023-11-25 14:56:25 +0000

    Final deploy

--- movember/settings.py
+++ movember/settings.py
@@ -30,7 +30,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = False
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://amiresponsive.co.uk/ https://responsivedesignchecker.com/ https://techsini.com/multi-mockup/'
